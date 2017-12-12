076a2243ff8860f0d7badc3bce8b4d1602bb77b0
hsolbrig <solbrig.harold@mayo.edu>
2017-12-12 15:05:48 -0600

    Adds test to detect unmapped fhir Primitive types
    This package makes sure that all fhir Primitives map to xsd data types.
    Fixes issue #9

---
+++ tests/fhir_tests/test_fhir_ttl.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2017, Mayo Clinic
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+#     list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#     Neither the name of the Mayo Clinic nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from rdflib import Graph, RDFS, BNode, OWL, XSD, URIRef
+
+from fhirtordf.rdfsupport.namespaces import FHIR
+from tests.utils.base_test_case import FHIRGraph
+
+
+class FHIRTTLTestCase(unittest.TestCase):
+
+    def is_xsd_primitive(self, prim: URIRef, g: Graph) -> bool:
+        print("testing: {}".format(prim))
+        for node in g.objects(prim, RDFS.subClassOf):
+            if isinstance(node, BNode) and g.value(node, OWL.onProperty) == FHIR.value:
+                base_type = g.value(node, OWL.allValuesFrom)
+                return str(base_type).startswith(str(XSD))
+        return False
+
+    def test_primitives(self):
+        """
+        FHIR periodically makes changes to the fhir data types.  This test determines whether there are any FHIR
+        primitive types that aren't anchored in the XML space
+        """
+        g = FHIRGraph()
+        tests = [self.is_xsd_primitive(p, g) for p in g.subjects(RDFS.subClassOf, FHIR.Primitive)]
+        self.assertTrue(all(tests))
+
+
+if __name__ == '__main__':
+    unittest.main()
